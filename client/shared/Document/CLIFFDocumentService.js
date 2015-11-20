d8026c8325912c5dcde30f75bf8ea8237c90b809
Ben Adams <darwinzero@gmail.com>
2015-11-20 22:36:50 +0000

    add new fields placeReferences, matchedPlaces

--- client/shared/Document/CLIFFDocumentService.js
+++ client/shared/Document/CLIFFDocumentService.js
@@ -2,6 +2,56 @@
 angular.module('pteraformer').service("CLIFFDocumentService", function (DocumentFactory, CLIFFService, $sanitize) {
   var cliffVersion = "cliff-2_3_0";
 
+  function clearCliffResults(doc) {
+    // remove parsing results
+    doc.parsingResults[cliffVersion] = {};
+
+    // get all cliff features in matched places
+    var cliffFeatureIdxSet = {};
+    for (var i = 0; i < doc.matchedPlaces.features.length; i++) {
+      if (doc.matchedPlaces.features[i].properties.source == cliffVersion) {
+        cliffFeatureIdxSet[i] = true;
+      }
+    }
+    
+    // remove cliff place references
+    var newPlaceReferences = [];
+    for (var i = 0; i < doc.placeReferences.length; i++) {
+      var featureIdx = doc.placeReferences[i].matchedPlacesFeatureIdx;
+      var newMatchedPlacesFeatureIdx = [];
+      for (var j = 0; j < featureIdx.length; j++) {
+        if (!(featureIdx[j] in cliffFeatureIdxSet)) { 
+          // if the feature index matched to this place reference is not cliff,
+          //  then keep it.
+          newMatchedPlacesFeatureIdx.push(featureIdx[j]);
+        }
+      }
+      // if not all matched places have been removed then push into new place references
+      if (newMatchedPlacesFeatureIdx.length > 0) {
+        newPlaceReferences.push({
+          "charIndex": doc.placeReferences[i].charIndex,
+          "string": doc.placeReferences[i].string,
+          "matchedPlacesFeatureIdx": newMatchedPlacesFeatureIdx
+        });
+      }
+    }
+    doc.placeReferences = newPlaceReferences;
+
+    // remove cliff features from matched places
+    var newMatchedPlacesFeatures = [];
+    for (var i = 0; i < doc.matchedPlaces.features[i]; i++) {
+      if (!(i in cliffFeatureIdxSet)) {
+        newMatchedPlacesFeatures.push(doc.matchedFeatures.features[i]);
+      }
+    }
+    doc.matchedPlaces = {
+      "type": "FeatureCollection",
+      "features": newMatchedPlacesFeatures
+    };
+
+    return doc;
+  };
+
   function addParsingResults(doc, results, updateMarkedUpText) {
     doc.parsingResults[cliffVersion] = results;
     var cliffPlacesFocus = results.places.focus;
@@ -9,6 +59,65 @@ angular.module('pteraformer').service("CLIFFDocumentService", function (Document
     var cliffOrganizations = results.organizations;
     var cliffPeople = results.people;
 
+    for (var i = 0; i < cliffPlacesMentions.length; i++) {
+      var charIndex = cliffPlacesMentions[i].source.charIndex;
+      var sourceString = cliffPlacesMentions[i].source.string;
+
+      // update the matchedPlaces object to include new feature
+      var feature = {
+        "type": "Feature",
+        "properties": {
+          "source": cliffVersion
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [cliffPlacesMentions[i].lon, cliffPlacesMentions[i].lat]
+        }
+      };
+      feature.properties.id = cliffPlacesMentions[i].id;
+      feature.properties.name = cliffPlacesMentions[i].name;
+      feature.properties.countryGeonameId = cliffPlacesMentions[i].countryGeonameId;
+      feature.properties.countryCode = cliffPlacesMentions[i].countryCode;
+      feature.properties.featureCode = cliffPlacesMentions[i].featureCode;
+      feature.properties.featureClass = cliffPlacesMentions[i].featureClass;
+      feature.properties.stateCode = cliffPlacesMentions[i].stateCode;
+      feature.properties.confidence = cliffPlacesMentions[i].confidence;
+      feature.properties.stateGeonameId = cliffPlacesMentions[i].stateGeonameId;
+      feature.properties.population = cliffPlacesMentions[i].population;
+
+      var featureIdx = doc.matchedPlaces.features.length;
+      // check if the feature is already in the list (by source + id)
+      for (var i = 0; i < doc.matchedPlaces.features; i++) {
+        if (doc.matchedPlaces.features[i].properties.source == cliffVersion && doc.matchedPlaces.features[i].id == feature.properties.id) {
+          featureIdx = i;
+          break;
+        }
+      }
+
+      // push the new feature if it was not found
+      if (featureIdx == doc.matchedPlaces.features.length)
+        doc.matchedPlaces.features.push(feature);
+
+      // now update place references
+      // check if there is already a place reference for the charIndex and string
+      var newPlaceReference = true;
+      for (int i = 0; i < doc.placeReferences.length; i++) {
+        if (doc.placeReferences.charIndex == charIndex && doc.placeReferences.string == sourceString) {
+          doc.placeReferences.matchedPlacesFeatureIdx.push(featureIdx);
+          newPlaceReference = false;
+          break;
+        }
+      }
+      if (newPlaceReference) {
+        doc.placeReferences.push({
+          "charIndex": charIndex,
+          "string": sourceString,
+          "matchedPlacesFeatureIdx": [featureIdx]
+        });
+      }
+    }
+
+    // TODO update to use placeReferences field
     if (updateMarkedUpText) {
       var markedUpText = doc.rawText;
       // insert the mentions as markup going from last to first
@@ -55,6 +164,7 @@ angular.module('pteraformer').service("CLIFFDocumentService", function (Document
     updateMarkedUpText = typeof updateMarkedUpText !== 'undefined' ? updateMarkedUpText : true;
     return CLIFFService.getParse(doc.rawText).then(
       function success(response) {
+        doc = clearCliffResults(doc);
         doc = addParsingResults(doc, response.data.results, updateMarkedUpText);
         return doc;
       },
