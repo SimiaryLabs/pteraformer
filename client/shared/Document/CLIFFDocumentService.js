0da759e46c2cb877362069c220da12191d39c57f
Ben Adams <darwinzero@gmail.com>
2015-11-20 09:26:33 +0000

    addParsingResult private function

--- client/shared/Document/CLIFFDocumentService.js
+++ client/shared/Document/CLIFFDocumentService.js
@@ -2,6 +2,36 @@
 angular.module('pteraformer').service("CLIFFDocumentService", function (DocumentFactory, CLIFFService, $sanitize) {
   var cliffVersion = "cliff-2_3_0";
 
+  function addParsingResults(doc, results, updateMarkedUpText) {
+    doc.parsingResults[cliffVersion] = results;
+    var cliffPlacesFocus = results.places.focus;
+    var cliffPlacesMentions = results.places.mentions;
+    var cliffOrganizations = results.organizations;
+    var cliffPeople = results.people;
+
+    if (updateMarkedUpText) {
+      var markedUpText = doc.rawText;
+      // insert the mentions as markup going from last to first
+      cliffPlacesMentions.sort(function(a,b) { return (a.source.charIndex > b.source.charIndex) ? 1 : ((b.source.charIndex > a.source.charIndex) ? -1 : 0); });
+      for (var i = cliffPlacesMentions.length-1; i >=0; i--) {
+        var charIndex = cliffPlacesMentions[i].source.charIndex;
+        var sourceString = cliffPlacesMentions[i].source.string;
+        var id = cliffPlacesMentions[i].id;
+        var lat = cliffPlacesMentions[i].lat;
+        var lon = cliffPlacesMentions[i].lon;
+        //var geoJson = '{"type":"Feature","geometry":{"type":"Point","coordinates":['+lon+','+lat+']}}'.replace(/"/g, "&quot;");
+        var geoJson = '{&quot;type&quot;:&quot;Feature&quot;,&quot;geometry&quot;:{&quot;type&quot;:&quot;Point&quot;,&quot;coordinates&quot;:['+lon+','+lat+']}}';
+        var markUpStart = '<place-reference id="'+id+'" geo="'+geoJson+'">';
+        var markUpEnd = "</place-reference>";
+        markedUpText = [markedUpText.slice(0, charIndex+sourceString.length), markUpEnd, markedUpText.slice(charIndex+sourceString.length)].join('');
+        markedUpText = [markedUpText.slice(0, charIndex), markUpStart, markedUpText.slice(charIndex)].join('');
+      }
+      doc.markedUpText = markedUpText;
+      doc.properties.modifyDate = Date.now();
+    }    
+    return doc;
+  }
+
   // return a new Document from a raw text string
   this.fromText = function(rawText) { 
     // sanitize it first so that we can trust to render later on
@@ -9,29 +39,7 @@ angular.module('pteraformer').service("CLIFFDocumentService", function (Document
     return CLIFFService.getParse(rawText).then(
       function success(response) {
         var doc = new DocumentFactory(rawText, "", {});
-        doc.addParsingResult(cliffVersion, response.data.results);
-        var cliffPlacesFocus = response.data.results.places.focus;
-        var cliffPlacesMentions = response.data.results.places.mentions;
-        var cliffOrganizations = response.data.results.organizations;
-        var cliffPeople = response.data.results.people;
-
-        var markedUpText = rawText;
-        // insert the mentions as markup going from last to first
-        cliffPlacesMentions.sort(function(a,b) { return (a.source.charIndex > b.source.charIndex) ? 1 : ((b.source.charIndex > a.source.charIndex) ? -1 : 0); });
-        for (var i = cliffPlacesMentions.length-1; i >=0; i--) {
-          var charIndex = cliffPlacesMentions[i].source.charIndex;
-          var sourceString = cliffPlacesMentions[i].source.string;
-          var id = cliffPlacesMentions[i].id;
-          var lat = cliffPlacesMentions[i].lat;
-          var lon = cliffPlacesMentions[i].lon;
-          var geoJson = '{"type":"Feature","geometry":{"type":"Point","coordinates":['+lon+','+lat+']}}'.replace(/\"/g, "&quot;");
-          var markUpStart = '<place-reference id="'+id+'" geo="'+geoJson+'">';
-          var markUpEnd = "</place-reference>";
-          markedUpText = [markedUpText.slice(0, charIndex+sourceString.length), markUpEnd, markedUpText.slice(charIndex+sourceString.length)].join('');
-          markedUpText = [markedUpText.slice(0, charIndex), markUpStart, markedUpText.slice(charIndex)].join('');
-        }
-        doc.markedUpText = markedUpText;
-        doc.properties.modifyDate = Date.now();
+        doc = addParsingResults(doc, response.data.results, true);
         return doc;
       },
       function error(response) {
@@ -47,32 +55,7 @@ angular.module('pteraformer').service("CLIFFDocumentService", function (Document
     updateMarkedUpText = typeof updateMarkedUpText !== 'undefined' ? updateMarkedUpText : true;
     return CLIFFService.getParse(doc.rawText).then(
       function success(response) {
-        doc.parsingResults[cliffVersion] = response.data.results;
-        var cliffPlacesFocus = response.data.results.places.focus;
-        var cliffPlacesMentions = response.data.results.places.mentions;
-        var cliffOrganizations = response.data.results.organizations;
-        var cliffPeople = response.data.results.people;
-
-        if (updateMarkedUpText) {
-          var markedUpText = doc.rawText;
-          // insert the mentions as markup going from last to first
-          cliffPlacesMentions.sort(function(a,b) { return (a.source.charIndex > b.source.charIndex) ? 1 : ((b.source.charIndex > a.source.charIndex) ? -1 : 0); });
-          for (var i = cliffPlacesMentions.length-1; i >=0; i--) {
-            var charIndex = cliffPlacesMentions[i].source.charIndex;
-            var sourceString = cliffPlacesMentions[i].source.string;
-            var id = cliffPlacesMentions[i].id;
-            var lat = cliffPlacesMentions[i].lat;
-            var lon = cliffPlacesMentions[i].lon;
-            //var geoJson = '{"type":"Feature","geometry":{"type":"Point","coordinates":['+lon+','+lat+']}}'.replace(/"/g, "&quot;");
-            var geoJson = '{&quot;type&quot;:&quot;Feature&quot;,&quot;geometry&quot;:{&quot;type&quot;:&quot;Point&quot;,&quot;coordinates&quot;:['+lon+','+lat+']}}';
-            var markUpStart = '<place-reference id="'+id+'" geo="'+geoJson+'">';
-            var markUpEnd = "</place-reference>"; 
-            markedUpText = [markedUpText.slice(0, charIndex+sourceString.length), markUpEnd, markedUpText.slice(charIndex+sourceString.length)].join('');
-            markedUpText = [markedUpText.slice(0, charIndex), markUpStart, markedUpText.slice(charIndex)].join('');
-          }
-          doc.markedUpText = markedUpText;
-          doc.properties.modifyDate = Date.now();
-        }
+        doc = addParsingResults(doc, response.data.results, updateMarkedUpText);
         return doc;
       },
       function error(response) {
